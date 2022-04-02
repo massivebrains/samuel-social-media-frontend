044c907cfd14688b5ec2781771745271035c527c
Olaiya Segun <vadeshayo@gmail.com>
2022-04-02 12:05:06 +0100

    Initial application commit

---
+++ components/FormButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    type="button"
+    v-on:click="submit"
+    class="
+      text-white
+      bg-blue-700
+      hover:bg-blue-800
+      focus:ring-4 focus:outline-none focus:ring-blue-300
+      font-medium
+      rounded-lg
+      text-sm
+      w-full
+      sm:w-auto
+      px-5
+      py-2.5
+      text-center
+      dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800
+    "
+  >
+    {{ label }}
+  </button>
+</template>
+<script>
+export default {
+  props: ["label", "submit"],
+};
+</script>
---
+++ components/Register.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="w-full md:flex md:justify-center mb-6">
+    <div class="m-12 w-1/2">
+      <h1 class="text-4xl mb-8">Social-Influenza</h1>
+      <FormInput
+        label="First Name"
+        type="text"
+        :form="form"
+        field="first_name"
+      />
+      <FormInput label="Last Name" type="text" :form="form" field="last_name" />
+      <FormInput
+        label="Email Address"
+        type="email"
+        :form="form"
+        field="email"
+      />
+      <FormInput
+        label="Phone Number"
+        type="number"
+        :form="form"
+        field="phone"
+      />
+      <FormInput
+        label="Twitter Handle"
+        type="text"
+        :form="form"
+        field="twitter_handle"
+      />
+      <FormInput
+        label="Instagram Handle"
+        type="text"
+        :form="form"
+        field="instagram_handle"
+      />
+      <FormInput
+        label="Password"
+        type="password"
+        :form="form"
+        field="password"
+      />
+      <FormInput
+        label="Confirm Password"
+        type="password"
+        :form="form"
+        field="password_confirmation"
+      />
+      <FormButton label="Register Now" :submit="submit" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      form: {
+        first_name: "",
+        last_name: "",
+        email: "",
+        phone: "",
+        twitter_handle: "",
+        instagram_handle: "",
+        password: "",
+        password_confirmation: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      alert(JSON.stringify(this.form));
+    },
+  },
+};
+</script>
